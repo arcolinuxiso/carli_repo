8a27c09a2352ec878e6081770cf13e18b022a606
Erik Dubois <erik.dubois@gmail.com>
2020-02-15 13:08:25 +0100

    zst for calamares

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -3,9 +3,8 @@
 rm carli_repo*
 
 echo "repo-add"
-#repo-add -s -n -R arcolinux_repo_testing.db.tar.gz *.pkg.tar.xz
 #repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
-repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.xz
+repo-add -n -R carli_repo.db.tar.gz *.pkg.tar.zst
 sleep 5
 #cp arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
 
