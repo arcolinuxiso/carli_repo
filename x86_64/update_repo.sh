e20f416d31e4fc43a2a65979084df4ec69743344
Erik Dubois <erik.dubois@gmail.com>
2020-09-20 10:45:48 +0200

    also database have been signed

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -4,7 +4,7 @@ rm carli_repo*
 
 echo "repo-add"
 #repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
-repo-add -n -R carli_repo.db.tar.gz *.pkg.tar.zst
+repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
 sleep 5
 #cp arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
 
