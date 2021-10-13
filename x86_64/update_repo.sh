46ac9b5b143520478c7906fc356f1267074f8f78
Erik Dubois <erik.dubois@gmail.com>
2021-10-13 18:45:44 +0200

    update

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -5,9 +5,20 @@ rm carli_repo*
 echo "repo-add"
 #repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
 repo-add -s -n -R carli_repo.db.tar.gz *.pkg.tar.zst
-sleep 5
-#cp arcolinux_repo_testing.db.tar.gz arcolinux_repo_testing.db
 
+sleep 1
+
+rm carli_repo.db
+rm carli_repo.db.sig
+
+rm carli_repo.files
+rm carli_repo.files.sig
+
+mv carli_repo.db.tar.gz carli_repo.db
+mv carli_repo.db.tar.gz.sig carli_repo.db.sig
+
+mv carli_repo.files.tar.gz carli_repo.files
+mv carli_repo.files.tar.gz.sig carli_repo.files.sig
 echo "####################################"
 echo "Repo Updated!!"
 echo "####################################"
